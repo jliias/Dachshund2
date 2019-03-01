59b8ae30c66c5eca4cebbba4262818a87133a2a9
Juha Liias <juha.liias@gmail.com>
2019-03-02 00:09:15 +0200

    prefabs and music

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,22 @@
+﻿// Unity3D C# script for spawning enemies
+// Author: Juha Liias
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+    // Array for different enemy prefabs
+    public GameObject[] enemies;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
