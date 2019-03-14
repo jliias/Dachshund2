7a7c44f494d1348558e10034b121c5a44d4ded4a
Juha Liias <juha.liias@gmail.com>
2019-03-14 23:13:31 +0200

    Virtual joystick added for evaluation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,9 @@
 ﻿// Unity 3D C# script for player actions
 // Author: Juha Liias
 // ToDO: implement player movement
+//       Currently testing virtual joystick, so all movement related
+//       parts are commented out for this version. See separate 
+//       Joystick.cs script for more info
 
 using System.Collections;
 using UnityEngine;
@@ -10,7 +13,7 @@ public class Player : MonoBehaviour {
     // projectile prefab
     public GameObject myProjectile;
 
-    private Camera myCamera;
+    //private Camera myCamera;
 
     // Projectile spawn interval
     public float intervalTime = 0.1f;
@@ -23,59 +26,59 @@ public class Player : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        myCamera = FindObjectOfType<Camera>();
-        targetPos = this.transform.position;
+        //myCamera = FindObjectOfType<Camera>();
+        //targetPos = this.transform.position;
         StartCoroutine("SpawnProjectile");
 	}
 
-    private void Update()
-    {
-        if (false)
-        {
-            currentPos = this.transform.position;
-            if (Input.GetMouseButton(0))
-            {
-                Vector3 mousePos = Input.mousePosition;
-                mousePos.z = -10f;
-                targetPos = myCamera.ScreenToWorldPoint(mousePos);
-                targetPos.z = currentPos.z;
-                Debug.Log("currentPos: " + currentPos);
-                Debug.Log("target: " + targetPos);
-                transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
-            }
-        }
+    //private void Update()
+    //{
+    //    if (false)
+    //    {
+    //        currentPos = this.transform.position;
+    //        if (Input.GetMouseButton(0))
+    //        {
+    //            Vector3 mousePos = Input.mousePosition;
+    //            mousePos.z = -10f;
+    //            targetPos = myCamera.ScreenToWorldPoint(mousePos);
+    //            targetPos.z = currentPos.z;
+    //            Debug.Log("currentPos: " + currentPos);
+    //            Debug.Log("target: " + targetPos);
+    //            transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+    //        }
+    //    }
 
-    }
+    //}
 
-    // Following four functions are used with
-    // virtual joystick(s), depending on configuration
-    public void goLeft() {
-        currentPos = this.transform.position;
-        currentPos.x -= speed * Time.deltaTime;
-        transform.position = currentPos;
-        Debug.Log("LEFT!");
-    }
+    //// Following four functions are used with
+    //// virtual joystick(s), depending on configuration
+    //public void goLeft() {
+    //    currentPos = this.transform.position;
+    //    currentPos.x -= speed * Time.deltaTime;
+    //    transform.position = currentPos;
+    //    Debug.Log("LEFT!");
+    //}
 
-    public void goRight() {
-        currentPos = this.transform.position;
-        currentPos.x += speed * Time.deltaTime;
-        transform.position = currentPos;
-        Debug.Log("RIGHT!");
-    }
+    //public void goRight() {
+    //    currentPos = this.transform.position;
+    //    currentPos.x += speed * Time.deltaTime;
+    //    transform.position = currentPos;
+    //    Debug.Log("RIGHT!");
+    //}
 
-    public void goUp() {
-        currentPos = this.transform.position;
-        currentPos.y += speed * Time.deltaTime;
-        transform.position = currentPos;
-        Debug.Log("UP!");
-    }
+    //public void goUp() {
+    //    currentPos = this.transform.position;
+    //    currentPos.y += speed * Time.deltaTime;
+    //    transform.position = currentPos;
+    //    Debug.Log("UP!");
+    //}
 
-    public void goDown() {
-        currentPos = this.transform.position;
-        currentPos.y -= speed * Time.deltaTime;
-        transform.position = currentPos;
-        Debug.Log("DOWN!");
-    }
+    //public void goDown() {
+    //    currentPos = this.transform.position;
+    //    currentPos.y -= speed * Time.deltaTime;
+    //    transform.position = currentPos;
+    //    Debug.Log("DOWN!");
+    //}
 
     // Use repetitive coroutine for spawn projectiles with defined interval
     IEnumerator SpawnProjectile() {
